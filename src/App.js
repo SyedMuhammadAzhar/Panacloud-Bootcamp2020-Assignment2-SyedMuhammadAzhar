418870224329297c237cba0bd368901c99182031
Syed Muhammad Azhar <syed.moh.azhar.98@gmail.com>
2021-05-04 07:58:45 +0500

    Updated React v1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br>
+        </br>
+
+        <h3>Updated by Syed Muhammad Azhar</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
